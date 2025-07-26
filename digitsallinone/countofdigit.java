8212cafe5a623fe7477dbea0abddf1670abd62cf
velmurugan122004 <vm4333332@gmail.com>
2025-07-26 14:19:19 +0530

    Count of digit

---
+++ digitsallinone/countofdigit.java
@@ -0,0 +1,19 @@
+package digitsallinone;
+import java.util.*;
+public class countofdigit {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("ENTER THE NUMBER : ");
+		int n=s.nextInt();
+		int count=0;
+		while(n!=0)
+		{
+			n/=10;
+			count++;
+		}
+		System.out.println("COUNT OF THE GIVEN DIGIT IS : "+count);
+
+	}
+
+}
