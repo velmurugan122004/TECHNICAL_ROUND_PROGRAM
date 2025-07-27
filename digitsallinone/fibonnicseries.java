0528390a8a66f0735d7de9526f4dde3d38716f06
velmurugan122004 <vm4333332@gmail.com>
2025-07-27 20:44:22 +0530

    fibonnic series program

---
+++ digitsallinone/fibonnicseries.java
@@ -0,0 +1,21 @@
+package digitsallinone;
+import java.util.*;
+public class fibonnicseries {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print("ENTER THE NUMBER : ");
+		int n=s.nextInt();
+		int a=0,b=1,c=0;
+		System.out.println("FIBONNIC SERIES..:-");
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(a +" ");
+			c=a+b;
+			a=b;
+			b=c;
+		}
+;
+	}
+
+}
