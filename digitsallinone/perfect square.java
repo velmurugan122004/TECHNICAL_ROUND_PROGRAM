e308dc70fe6100d71c81f4cf88f4d853aa62ffb6
M Vel Murugan <143987627+velmurugan122004@users.noreply.github.com>
2025-08-01 12:35:22 +0530

    Create perfect square.java
    Checks the number is perfect square or not

---
+++ digitsallinone/perfect square.java
@@ -0,0 +1,41 @@
+package digitsallinone;
+import java.util.*;
+class perfectsquare
+  {
+    public static boolean isperfect(int n)
+    {
+      int first=1,last=n,mid,square;
+      while(first<=last)
+      {
+          mid=first+(last-first)/2;
+          square=mid*mid;
+          if(square==n)
+          {
+            return true;
+          }
+          else if(square<n)
+          {
+            first=mid+1;
+          }
+          else{
+            last=mid-1;
+          }
+      }
+      return false;
+    }
+        
+    public static void main (String ar[])
+    {
+      Scanner s=new Scanner(System.in);
+      System.out.print("Enter THE NUMBER : ");
+      int n=s.nextInt();
+      if(isperfect(n))
+      {
+        System.out.println("THE GIVEN NUMBER IS PERFECT SQUARE NUMBER");
+      }
+      else{
+        System.out.println("THE GIVEN NUMBER IS NOT PERFECT SQUARE NUMBER ");
+      }
+      s.close();
+    }
+  }
